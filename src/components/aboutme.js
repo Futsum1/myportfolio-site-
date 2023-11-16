2d079fe1c78c71f186bd79be74f99ae3b373011c
Futsum Nemariam <futsumnemariam2@gmil.com>
2023-11-15 16:58:31 -0800

    update

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,59 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Layout, Content, Grid } from 'react-mdl';
 
 class About extends Component {
     render() {
-      return(
-  <div>
-  <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-       
-               <div style={{width: '100%', margin: '35px'}} class="card-body text-center"> 
-               <img src="https://i.imgur.com/CdlKyhF.jpg" alt="Cinque Terre" class="rounded-circle"  width="200" height="200" position="fixed"/>
-                    <h5 class="card-title mt-2 mb-1" style={{color: 'white'}}>Futsum Nemariam</h5> <span style={{color: 'white'}} class="fs-2 mb-3 font-weight-bold">Software Engineer</span>
-                
-                    <div class="container mt-5 d-flex justify-content-center ">
-    <div class="maincard p-3">
-        <div class="thecard">
-            
-            
-        </div>
-    </div>
-</div>
-                    
-                </div>  
-         <Content />
+        return (
+            <div>
+                <div style={{ height: '300px', position: 'relative' }}>
+                    <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover' }}>
 
-    </Layout>
-    </div>
-<div style={{ marginLeft: '2em'}}>
-<h2 style={{  fontSize: '2em', textAlign: 'center'}}>About Me.</h2>
+                        <div style={{ width: '100%', margin: '35px' }} class="card-body text-center">
+                            <img src="https://i.imgur.com/CdlKyhF.jpg" alt="Cinque Terre" class="rounded-circle" width="200" height="200" position="fixed" />
+                            <h5 class="card-title mt-2 mb-1" style={{ color: 'white' }}>Futsum Nemariam</h5> <span style={{ color: 'white' }} class="fs-2 mb-3 font-weight-bold">Software Engineer</span>
 
-<ul class="list-inline" style={{  fontSize: '2em', textAlign: 'center', color: 'blue'}}>
-    <li class="list-inline-item"><a href="https://www.facebook.com/futsum.melake"><i class="fa fa-facebook fa-2x facebook"></i></a></li>
-    <li class="list-inline-item"><a href="https://twitter.com/Futsum62355355"><i class="fa fa-twitter fa-2x twitter"></i></a></li>
-    <li class="list-inline-item"><a href="https://www.linkedin.com/in/futsumne2020/"><i class="fa fa-linkedin fa-2x linkedin"></i></a></li>
-    <li class="list-inline-item"><a href="https://github.com/Futsum1"><i class="fa fa-github fa-2x github"></i></a></li>
-</ul> 
-<p style={{  fontSize: '1.5em', textAlign: 'center'}}> </p>
-<hr />            
-
-<Grid className="demo-grid-1">
-        <p style={{  fontSize: '1.1em', textAlign: 'center', fontStyle: "italic"}} col={14}>My name is Futsum Nemariam. 
-        <br /> 
-I am a Junior Full-Stack Software Developer. I enjoy being challenged and engaging with coding to identify potential issues earlier in the development cycle and also attentive to the need for continuous process improvements. I am passionate extremely interested in full-stack software engineering. I have always been motivated by creative projects, teamwork, and being able to learn new skills.
-        {/* I am a Respectful self-motivator positive about every aspect of life. I'd really describe myself as a person with a versatile skill-set, I’m recently a graduate of a software engineer who is eager to apply the knowledge and skills I’ve learned in a work environment I’d like to build a long term career that lets me focus on professional growth. I am passionate about  Full-Stack web development. */}
-        </p>
-
-</Grid>
+                            <div class="container mt-5 d-flex justify-content-center ">
+                                <div class="maincard p-3">
+                                    <div class="thecard">
 
 
-    <Content />
-</div>
-</div>
+                                    </div>
+                                </div>
+                            </div>
 
-      )
+                        </div>
+                        <Content />
+
+                    </Layout>
+                </div>
+                <div style={{ marginLeft: '2em' }}>
+                    <h2 style={{ fontSize: '2em', textAlign: 'center' }}>About Me.</h2>
+
+                    <ul class="list-inline" style={{ fontSize: '2em', textAlign: 'center', color: 'blue' }}>
+                        <li class="list-inline-item"><a href="https://www.facebook.com/futsum.melake"><i class="fa fa-facebook fa-2x facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="https://twitter.com/Futsum62355355"><i class="fa fa-twitter fa-2x twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="https://www.linkedin.com/in/futsumne2020/"><i class="fa fa-linkedin fa-2x linkedin"></i></a></li>
+                        <li class="list-inline-item"><a href="https://github.com/Futsum1"><i class="fa fa-github fa-2x github"></i></a></li>
+                    </ul>
+                    <p style={{ fontSize: '1.5em', textAlign: 'center' }}> </p>
+                    <hr />
+
+                    <Grid className="demo-grid-1">
+                        <p style={{ fontSize: '1.1em', textAlign: 'center', fontStyle: "italic" }} col={14}>My name is Futsum Nemariam.
+                            <br />
+                            I'm an Experienced Software Engineer, deeply immersed in the realms of cloud computing and DevOps principles. Known for my meticulous approach, I'm passionate about the intricacies of software and cloud engineering. I thrive on tackling coding challenges, unraveling problems, and immersing myself in creative projects. As an effective team player with strong communication skills, I believe in the power of collaboration to elevate our work. Let's connect and explore how we can shape the future through technology!
+                            {/* I am a Respectful self-motivator positive about every aspect of life. I'd really describe myself as a person with a versatile skill-set, I’m recently a graduate of a software engineer who is eager to apply the knowledge and skills I’ve learned in a work environment I’d like to build a long term career that lets me focus on professional growth. I am passionate about  Full-Stack web development. */}
+                        </p>
+
+                    </Grid>
+
+
+                    <Content />
+                </div>
+            </div>
+
+        )
     }
-   }  
+}
 
-   export default About;
+export default About;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,113 +1,134 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import Education from './education';
 import Experience from './experience';
 import Skills from './skills';
 
 class Resume extends Component {
-    render() {
-      return(
-        <div>
-          <Grid>
-            <Cell col={4}>
-              <div style={{textAlign: 'center'}}> 
-               <img 
-               src="https://i.imgur.com/CdlKyhF.jpg" class="rounded-circle"  width="150" height="185" 
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{ textAlign: 'center' }}>
+              <img
+                src="https://i.imgur.com/CdlKyhF.jpg" class="rounded-circle" width="150" height="185"
                 alt="unsplash"
-                style={{height: '200px'}} 
-                />
-              </div>
-             
-             <h2 style={{paddingTop: '23m'}}>Futsum Nemariam</h2>
-             <h4 style={{color: 'grey'}}>Programmer</h4>
-             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-             <h6>Highly motivated and progress-focused software engineer.  I enjoy being challenged and engaging with coding to identify potential issues earlier in the development cycle and also attentive to the need for continuous process improvements. I am extremely interested in full-stack software engineering. I have always been motivated by creative projects, teamwork, and being able to learn new skills.</h6>
-             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-             <h5>Adress.</h5>
-             <p> Santa Rosa, California, 95403.</p>
-             <h5>Phone.</h5>
-             <p>(707) 571-9900</p>
-             <h5>Email.</h5>
-             <p>futsumnemariam2@gamil.com</p>
-             <h5>Web.</h5>
-             <p>Website.com</p>
-             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            </Cell>
-            <Cell className="resume-right-col" col={8}>
-             <h2>Education</h2>
-             <Education
-                  startYear={2020}
-                  endYear={2020}
-                  schoolName="GENERAL ASSEMBLY."
-                  schoolDescription="Bootcamp Graduate Software Engineer Immersive ."
-              />  
- 
-            <Education
-              startYear={20018}
-              endYear={2020}
-              schoolName="Santa Rosa Junior Collage."
-              schoolDescription="Computer Technology."
-             />
-            
-            <Education
-              startYear={2006}
-              endYear={2010}
-              schoolName="KEIH BAHRI."
-              schoolDescription="High School Graduate General Science."
-             />
-
-             <hr style={{borderTop: '3px solid #e22947'}} />
-
-             <h2>Experience</h2>
-
-            <Experience
-              startYear={2020}
-              endYear={2020}
-              jobName="GENERAL ASSEMBLY."
-              jobDescription="Software Engineer Immersive | 
-              Three-month, 500-hour full-time and full-stack program conducted in a remote setting, providing experience with the latest front- and back-end programming languages, tools, and methodologies.
-              Developed a portfolio of individual and collaborative in-class projects, including:"
+                style={{ height: '200px' }}
               />
-            <Experience
-            startYear={2018}
-            endYear={2020}
-            jobName="Inclusion Service."
-            jobDescription="Responded appropriately to the physical emotional and developmental needs of patients.
-            Obtained information about clients' medical history, drug history, complaints, and allergies, assistant."
+            </div>
+
+            <h2 style={{ paddingTop: '23m' }}>Futsum Nemariam</h2>
+            <h4 style={{ color: 'grey' }}>Programmer</h4>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            <h6>Highly motivated and progress-focused software engineer.  I enjoy being challenged and engaging with coding to identify potential issues earlier in the development cycle and also attentive to the need for continuous process improvements. I am extremely interested in full-stack software engineering. I have always been motivated by creative projects, teamwork, and being able to learn new skills.</h6>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            <h5>Adress.</h5>
+            <p> Santa Rosa, California, 95403.</p>
+            <h5>Phone.</h5>
+            <p>(707) 571-9900</p>
+            <h5>Email.</h5>
+            <p>futsumnemariam2@gamil.com</p>
+            <h5>Web.</h5>
+            <p>Website.com</p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+            <Education
+              startYear={2021}
+              endYear={2025}
+              schoolName="Santa Rosa JC."
+              schoolDescription="BA degree, in Computer Science (In progress) from SRJC."
             />
 
-             <Experience
-              startYear={2017}
-              endYear={2018}
-              jobName="Memorial Hospital."
-              jobDescription="Maintaining Implemented new technologies at medical centers enhances the overall safety of staff, patients, and visitors."
-              />
+            <Education
+              startYear={"Nov - 2022"}
+              schoolName="Certified Cloud Practitioner."
+              schoolDescription="AWS Re/Start Program."
+            />
 
-              <hr style={{borderTop: '3px solid #e22947'}} />
-              <h2>Skills</h2>
-              
-              <Skills
-                skill="JavaScript"
-                progress={90}
-                />
-                <Skills
-                  skill="HTML/CSS"
-                  progress={70}
-                  />
-                  <Skills
-                    skill="Node.Js"
-                    progress={45}
-                    />
-                    <Skills
-                      skill="React"
-                      progress={50}
-                      />
+            <Education
+              startYear={" Nov -2020"}
+              schoolName="Certified Software Engineer Immersive."
+              schoolDescription="General Assembly."
+            />
 
-          </Cell>    
-          </Grid>
-        </div>
-      )
-    }
-   }  
+            <hr style={{ borderTop: '3px solid #e22947' }} />
 
-   export default Resume;
+            <h2>Experience</h2>
+
+            <Experience
+              startYear={"08 - 2021"}
+              endYear={"07 - 2020"}
+              jobName="Front-End Developer, Bitwise Industries."
+              jobDescription="• Developed user-friendly websites and apps using HTML, CSS, JavaScript, Node.js, jQuery, and React.js.
+              • Experienced in both Waterfall and Agile methodologies throughout the Software Development Life Cycle
+              (SDLC).
+              • Applied Object-Oriented Programming concepts and implemented Object-Oriented JavaScript.
+              • Prof iciently worked with various JavaScript MVC frameworks, including Node.js, React.js, and Express.js.
+              • Skillfully applied popular React.js workflows, including Redux and Flux.
+              • Utilized IDEs such as Visual Studio Code for application development.
+              • Led the adoption of test-driven development, demonstrating proactive engagement and proficiency with
+              Grunt/Gulp testing tools.
+              • Demonstrated proficiency in working with databases, specifically MongoDB and SQL.
+              • Proficient in version control tools like GIT and GitHub for source code version management.
+              "
+            />
+            <Experience
+              startYear={"07- 2020"}
+              endYear={"07 - 2021"}
+              jobName="AWS Cloud DevOps, Alpha Works."
+              jobDescription="Configured and deployed AWS servers, increasing scalability by 50% and reducing response time by 40%.
+            • Leveraged cloud computing technologies, resulting in a 50% reduction in infrastructure costs and
+            improving resource utilization by 30%.
+            • Developed a highly available and fault-tolerant cloud-based application with 99.99% uptime and less than
+            1% downtime.
+            • Designed a multi-region cloud architecture for improved availability and fault tolerance, ensuring
+            uninterrupted service during region-specific outages.
+            • Collaborated on AWS server configuration for a 50% scalability boost and 40% faster response time.
+            • Implemented robust security policies and achieved 100% compliance with industry standards, successfully
+            passing all security audits.
+            ."
+            />
+
+            <Experience
+              startYear={"01 - 2021"}
+              endYear={"07 - 2020"}
+              jobName="Software Engineer Intern, General Assembly."
+              jobDescription="Contributed to real-world projects, collaborating on development, testing, maintenance, and debugging with senior developers.
+              • Gained hands-on experience in the SDLC, including requirements gathering and GitHub management.
+              • Collaborated on React.js components, forms, events, routers, and Redux, applying Agile methodologies
+              for UI development.
+              • Developed web applications, gaining exposure to HTML, CSS, JavaScript, and client-server technologies.
+              • Implemented Security Protocols, enhancing data protection through encryption and authentication measures in web applications.."
+            />
+
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            <h2>Skills</h2>
+
+            <Skills
+              skill="JavaScript"
+              progress={80}
+            />
+            <Skills
+              skill="HTML/CSS"
+              progress={80}
+            />
+            <Skills
+              skill="Node.Js"
+              progress={50}
+            />
+            <Skills
+              skill="React"
+              progress={60}
+            />
+
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Resume;
